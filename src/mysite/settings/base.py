f7369edbb2420fbc8e4bded25350aacb835ca2f9
Justin Menga <justin.menga@gmail.com>
2015-10-18 15:15:17 +1300

    Remove static_url and media_url

--- src/mysite/settings/base.py
+++ src/mysite/settings/base.py
@@ -96,8 +96,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = '/var/www/mysite/static'
-MEDIA_ROOT = '/var/www/mysite/media'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
