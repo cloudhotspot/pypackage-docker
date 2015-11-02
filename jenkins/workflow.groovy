d02f68c41cf8fcaac04e72674101198512ed5b0d
Justin Menga <justin.menga@cloudhotspot.co>
2015-11-02 14:17:41 +1300

    Remove unneeded git url

--- jenkins/workflow.groovy
+++ jenkins/workflow.groovy
@@ -1,8 +1,4 @@
-def src = 'https://github.com/cloudhotspot/pypackage-docker.git'
-
 node {
-    // git url: src
-
     stage 'Build Base Images'
     sh 'make image docker/base'
     sh 'make image docker/dev'
