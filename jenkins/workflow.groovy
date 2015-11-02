023ed34f420056cacb8c59f637f5b0c6bba029c7
Justin Menga <justin.menga@cloudhotspot.co>
2015-11-02 14:41:49 +1300

    Add exception handling to try and capture test results under failure

--- jenkins/workflow.groovy
+++ jenkins/workflow.groovy
@@ -1,21 +1,26 @@
 def src = 'https://github.com/cloudhotspot/pypackage-docker.git'
 
 node {
-    git url: src
+    try {
+        git url: src
 
-    stage 'Build Base Images'
-    sh 'make image docker/base'
-    sh 'make image docker/dev'
-    sh 'make image docker/agent'
+        stage 'Build Base Images'
+        sh 'make image docker/base'
+        sh 'make image docker/dev'
+        sh 'make image docker/agent'
 
-    stage 'Run Unit/Integration Tests'
-    sh 'make test'
+        stage 'Run Unit/Integration Tests'
+        sh 'make test'
 
-    stage 'Build Application Artefacts'
-    sh 'make build'
+        stage 'Build Application Artefacts'
+        sh 'make build'
 
-    stage 'Build Runtime Artefacts'
-    sh 'make release'
+        stage 'Build Runtime Artefacts'
+        sh 'make release'
+    } catch {
+        step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
+    } finally {
+        step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
+    }
 
-    step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
 }
