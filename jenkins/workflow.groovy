f29b024d7094f69f5c8efbd1092fe5b03ce7320d
Justin Menga <justin.menga@cloudhotspot.co>
2015-11-02 14:43:29 +1300

    Remove broken exception handling

--- jenkins/workflow.groovy
+++ jenkins/workflow.groovy
@@ -1,26 +1,21 @@
 def src = 'https://github.com/cloudhotspot/pypackage-docker.git'
 
 node {
-    try {
-        git url: src
+    git url: src
 
-        stage 'Build Base Images'
-        sh 'make image docker/base'
-        sh 'make image docker/dev'
-        sh 'make image docker/agent'
+    stage 'Build Base Images'
+    sh 'make image docker/base'
+    sh 'make image docker/dev'
+    sh 'make image docker/agent'
 
-        stage 'Run Unit/Integration Tests'
-        sh 'make test'
+    stage 'Run Unit/Integration Tests'
+    sh 'make test'
 
-        stage 'Build Application Artefacts'
-        sh 'make build'
+    stage 'Build Application Artefacts'
+    sh 'make build'
 
-        stage 'Build Runtime Artefacts'
-        sh 'make release'
-    } catch {
-        step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
-    } finally {
-        step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
-    }
+    stage 'Build Runtime Artefacts'
+    sh 'make release'
 
+    step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
 }
