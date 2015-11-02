6d23a08c96ba0728fa50e619429b4d620d9940dd
Justin Menga <justin.menga@cloudhotspot.co>
2015-11-02 16:01:18 +1300

    Add error handling for test failures

--- jenkins/workflow.groovy
+++ jenkins/workflow.groovy
@@ -8,9 +8,14 @@ node {
     sh 'make image docker/dev'
     sh 'make image docker/agent'
 
-    stage 'Run Unit/Integration Tests'
-    sh 'make test'
-
+    try {
+        stage 'Run Unit/Integration Tests'
+        sh 'make test'
+    } catch(all) {
+        step([$class: 'JUnitResultArchiver', testResults: '**/src/*.xml'])
+        error 'Test Failure'
+    }
+    
     stage 'Build Application Artefacts'
     sh 'make build'
 
