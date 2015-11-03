16ac963e24f39c287234ffea38fe72d96786d060
Justin Menga <justin.menga@gmail.com>
2015-11-04 06:39:40 +1300

    Test commit

--- jenkins/images.groovy
+++ jenkins/images.groovy
@@ -1,1 +1,10 @@
-images.groovy
+def src = 'https://github.com/cloudhotspot/pypackage-docker.git'
+
+node {
+    git url: src
+
+    stage 'Build Base Images'
+    sh 'make image docker/base'
+    sh 'make image docker/dev'
+    sh 'make image docker/agent'
+}
