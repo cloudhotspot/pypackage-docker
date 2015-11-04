fdec05c2f00dfeb6bc247d29872e5289332da56a
Justin Menga <justin.menga@gmail.com>
2015-11-05 02:18:03 +1300

    Uncomment tests

--- docker/base/dockerfile_spec.rb
+++ docker/base/dockerfile_spec.rb
@@ -10,15 +10,15 @@ describe "Dockerfile" do
     set :docker_image, image.id
   end
 
-  # it "installs the correct version of Ubuntu" do 
-  #   expect(os_version).to include("Ubuntu 14")
-  # end
+  it "installs the correct version of Ubuntu" do 
+    expect(os_version).to include("Ubuntu 14")
+  end
 
   it "installs required packages" do
     expect(package("python")).to be_installed
   end
 
-  # def os_version
-  #   command("lsb_release -a").stdout
-  # end
+  def os_version
+    command("lsb_release -a").stdout
+  end
 end
